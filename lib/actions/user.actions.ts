0349dd7016a202b361b27bc61efb773bb402526d
Nutt Chokwittaya <65862558+CHNUTT@users.noreply.github.com>
2024-06-02 22:14:57 -0500

    Issue 13 (#25)
    * ISSUE-13: Implement Logout feature
    
    * fix login function
    
    ---------
    
    Co-authored-by: root <root@Moonoi-Bebeb>

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -9,9 +9,16 @@ export const signIn = async (userData: signInProps) => {
   try {
     const { email, password } = userData;
     const { account } = await createAdminClient();
-    const response = await account.createEmailPasswordSession(email, password);
+    const session = await account.createEmailPasswordSession(email, password);
 
-    return parseStringify(response);
+    cookies().set('appwrite-session', session.secret, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: true,
+    });
+
+    return parseStringify(session);
   } catch (error) {
     console.log(error);
   }
